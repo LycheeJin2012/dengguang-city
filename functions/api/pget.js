7d2be472818650f525d786afaad557788e04ea1b
LycheeJin2012 <deploy@lycheejin2012.local>
2026-08-17 07:03:03 +0800

    test: rename dm.js to dmsg.js

--- functions/api/pget.js
+++
@@ -1,6 +0,0 @@
-// 极简测试 - 看是不是文件本身的问题
-export async function onRequestGet() {
-  return new Response(JSON.stringify({ ok: true, test: 'pinfo works' }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
